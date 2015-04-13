078dfdb6efdb9edab2c18b4c4eba1b3083cfd561
Leandro Cerencio <cerencio@yahoo.com.br>
2015-04-13 18:55:44 -0300

    Changing shape types

--- src/Hasktris/Shapes.hs
+++ src/Hasktris/Shapes.hs
@@ -1,10 +1,25 @@
 module Hasktris.Shapes ( cwRotate
                        , ccwRotate
-                       , shape)
+                       , Shape, L, Node)
 
 where
 
 import           Data.List (transpose)
+
+data Shape = L | Node Shape Shape Shape Shape deriving  (Show, Eq)
+
+tetriminos :: [Shape]
+tetriminos = [ Node L
+                    (Node L L
+                            (Node L L L L)
+                            L)
+                    L
+                    (Node L L L L)
+             , Node L
+                    (Node L L L
+                              (Node L L L L))
+                    (Node L L L L)
+                    L ]
 
 shape :: [[[Char]]]
 shape = [ [ [' ', '#', ' ']
